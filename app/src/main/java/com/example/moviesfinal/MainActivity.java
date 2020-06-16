0e12da0deaf9f77a8f43757f46c3c0e0eb81201e
freakcoder1 <35484642+freakcoder1@users.noreply.github.com>
2020-06-16 12:16:44 +0530

    hi there!! :)

--- app/src/main/java/com/example/moviesfinal/MainActivity.java
+++ app/src/main/java/com/example/moviesfinal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.moviesfinal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,10 +9,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
 
 import java.io.IOException;
 import java.net.URL;
@@ -24,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private Context context;
 
     private ImageView imageView;
+  private TextView title;
+    private Movie[] Movies;
 
 
     @Override
@@ -31,7 +40,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView)findViewById(R.id.myImage);
+        String SORT_BY_POPULAR = "http://api.themoviedb.org/3/movie/popular?api_key=";
+        String SORT_BY_TOP_RATING = "http://api.themoviedb.org/3/movie/top_rated?api_key=";
+
+        imageView = (ImageView) findViewById(R.id.myImage);
+        title = (TextView) findViewById(R.id.movieName);
 
         // Execute AsyncTask to get data from the API
         // send sort criteria (popular or top_rated)
@@ -39,15 +52,45 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.dear_RecyclerView);
 
         // use a linear layout manager
-        layoutManager = new GridLayoutManager(this,3);
+        layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
 
         // specify an adapter (see also next example)
-        mAdapter = new RecyclerViewAdapter(myDataset);
+        mAdapter = new RecyclerViewAdapter(context,Movies);
         recyclerView.setAdapter(mAdapter);
     }
-    //implementing image library Picasso
+
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+        if (menuItemThatWasSelected == R.id.action_settings) {
+            Context context = MainActivity.this;
+            String message = "Settings Clicked";
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+            switch (menuItemThatWasSelected){
+                case R.id.popular:
+                    Toast.makeText(getApplicationContext(),"Most Popular Movies option selected",Toast.LENGTH_LONG).show();
+                    return true;
+                case R.id.topRated:
+                    Toast.makeText(getApplicationContext(),"High Selected",Toast.LENGTH_LONG).show();
+                    return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    //implementing image library Picasso
+
     public void onClick(View view) {
 
         Picasso.get().load("http://image.tmdb.org/t/p/w185").into(imageView);
@@ -65,13 +108,30 @@ public class MainActivity extends AppCompatActivity {
 
                 // fetch movies from the API
                 String result = NetworkUtils.getResponseFromHttpUrl(url);
+                return result;
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
             return null;
         }
-    }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                Movie[] Movies = JsonUtils.getMovieInformationsFromJson(MainActivity.this, s);
+
+                layoutManager = new GridLayoutManager(MainActivity.this, 2);
+                recyclerView.setLayoutManager(layoutManager);
+
+                mAdapter = new RecyclerViewAdapter(MainActivity.this, Movies);
+                recyclerView.setAdapter(mAdapter);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
 
 
     }
+}
--- app/src/main/java/com/example/moviesfinal/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/moviesfinal/RecyclerViewAdapter.java
@@ -12,49 +12,31 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
 
-    private ArrayList<Movie> Movies = new ArrayList<>();
-
+    private Movie[] Movies;
     Context context;
 
-    public RecyclerViewAdapter(Context context) {
-
-        this.context = context;
-
-    }
-
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView MovietextView;
-
-        public MyViewHolder(TextView view) {
-            super(view);
-            MovietextView = view;
-        }
-    }
-
     // Provide a suitable constructor (depends on the kind of dataset)
-    public RecyclerViewAdapter(String[] myDataset) {
-        Movie = myDataset;
+    public RecyclerViewAdapter(Context context,Movie[] myDataset) {
+        this.Movies = myDataset;
+        this.context=context;
     }
+
+
 
     // Create new views (invoked by the layout manager)
     @NonNull
     @Override
-    public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public  RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // create a new view
 
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_details,parent,false);
-        ViewHolder holder= new ViewHolder(view);
-        MyViewHolder viewHolder = new MyViewHolder(view);
-        return viewHolder;
+        // ViewHolder holder= new ViewHolder(view);
+        MyViewHolder ViewHolder= new MyViewHolder(view);
+        return ViewHolder;
 
     }
 
@@ -63,13 +45,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        Movie mov = Movies.get(position);
-
-
-
-        Picasso.with(context)
-                .load(mov.get(position).getPoster())
-                .into(holder.myImage);
+        Movie mov = Movies[position];
+        holder.Title.setText(mov.getTitle());
+        Picasso.get().load(mov.getPoster()).into(holder.image);
 
 
     }
@@ -77,15 +55,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return Movies.size();
+        return Movies.length;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         private ImageView image;
         private TextView Title;
 
 
-        public ViewHolder(View itemView) {
+        public  MyViewHolder(View itemView) {
             super(itemView);
             image = (ImageView) itemView.findViewById(R.id.myImage);
             Title = (TextView) itemView.findViewById(R.id.movieName);
